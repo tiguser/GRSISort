ad59db2a7da565e677c324e05dc07cc612cb42eb
pcbend <pcbend@gmail.com>
2015-08-22 11:56:38 -0400

    TTigressData update.

--- libraries/TGRSIAnalysis/TTigress/TTigressData.cxx
+++ libraries/TGRSIAnalysis/TTigress/TTigressData.cxx
@@ -7,49 +7,32 @@
 
 bool TTigressData::fIsSet = false;
 
-TTigressData::TTigressData()	{	
-	Clear();
+TTigressData::TTigressData()  {  
+  Clear();
 }
 
-TTigressData::~TTigressData()	{	}
+TTigressData::~TTigressData()  {  }
 
 
-void TTigressData::Clear(Option_t *opt)	{
+void TTigressData::Clear(Option_t *opt)  {
 
-	fCoreSet.clear();
+  fCoreSet.clear();
 
-	fIsSet = false;
+  fIsSet = false;
 
-	fClover_Nbr.clear();
-	fCore_Nbr.clear();
-	fCore_Eng.clear();
-	fCore_Chg.clear();
-	fCore_TimeCFD.clear();
-	fCore_TimeLED.clear();
-	fCore_Time.clear();
+  fClover_Nbr.clear();
+  fCore_Nbr.clear();
+  fCore_Frag.clear();
 
-	for(int x=0;x<fCore_Wave.size();x++)	{
-		fCore_Wave[x].clear();
-	}
-	fCore_Wave.clear();
-
-	fSeg_Clover_Nbr.clear();
-	fSeg_Core_Nbr.clear();
-	fSegment_Nbr.clear();	
-	fSegment_Eng.clear();
-	fSegment_TimeCFD.clear();
-	fSegment_TimeLED.clear();
-	fSegment_Time.clear();
-
-	for(int x=0;x<fSegment_Wave.size();x++)	{
-		fSegment_Wave[x].clear();	
-	}
-	fSegment_Wave.clear();
+  fSeg_Clover_Nbr.clear();
+  fSeg_Core_Nbr.clear();
+  fSegment_Nbr.clear();  
+  fSegment_Frag.clear();
 }
 
-void TTigressData::Print(Option_t *opt)	{
-	// not yet written.
-	printf("not yet written.\n");
+void TTigressData::Print(Option_t *opt) const {
+  // not yet written.
+  printf("not yet written.\n");
 }
 
 
